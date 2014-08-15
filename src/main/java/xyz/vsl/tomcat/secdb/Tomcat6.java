5592e88dcd50e08ba9012ab5c18cadeaa72a79bf
vsl1978 <vsl1978@gmail.com>
2014-08-15 10:53:34 +0400

    update help messages

--- src/main/java/xyz/vsl/tomcat/secdb/Tomcat6.java
+++ src/main/java/xyz/vsl/tomcat/secdb/Tomcat6.java
@@ -14,6 +14,8 @@ public class Tomcat6 extends BaseObjectFactory implements ObjectFactory {
 
     @Override
     public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) throws Exception {
+        if (!(obj instanceof ResourceRef))
+            return null;
         ResourceRef resourceRef = decryptResourceRef((ResourceRef) obj);
         return new org.apache.tomcat.dbcp.dbcp.BasicDataSourceFactory().getObjectInstance(resourceRef, name, nameCtx, environment);
     }
